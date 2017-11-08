c1c5756739e071b5da04128d1942cd761e90c43a
nmask <tzc@maskghost.com>
2017-11-08 15:35:59 +0800

    update

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='CreateRe',
+    version='1.0.0',
+    description='To Create Re Python project',
+    url='https://github.com/tengzhangchao/CreateRe',
+    author='nMask',
+    author_email='tzc@maskghost.com',
+    classifiers=[
+	'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
